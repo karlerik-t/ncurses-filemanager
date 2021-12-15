fdf5eda2da71752a48293b63725096e2ea9bc8b9
karlerik-t <karlerik.toomemagi@gmail.com>
2021-12-15 17:59:01 +0200

    lisatud kausta maht. parandatud: pikad failinimed, tähestikuline järjekord suurte algustähtede puhul, nähtamatu viimane fail ekraanil, kustutamise ekraan (+ pikk failinimi kustutamisel)

--- file_manager.py
+++ file_manager.py
@@ -16,10 +16,6 @@ IMG_TEXT_BG = -1
 ETC_TEXT_FG = 15
 ETC_TEXT_BG = -1
 
-# global muutujad
-current_files = None
-selected = None
-
 def files(path="."):
     # dict failitüüpide (kaust, pilt, muu) jaoks
     contents = {}
@@ -44,28 +40,42 @@ def files(path="."):
     contents[3] = etc
     return contents       
 
+# pikkade kausta/failide nimede jaoks
+def short_el(el, n_cols):
+    if len(el) >= int(n_cols * 0.5):
+        return el[:int(n_cols * 0.5)] + "..."
+    else:
+        return el
+
 # prindib kausta sisu tähestikulises järjekorras (võttes arvesse prioriteete), prioriteet: kaustad -> pildid -> muu. 
 def print_menu(main_window, current_row):
     main_window.clear()
-    height = n_rows - 3
+    # koha arvutamiseks, kui current_row läheb ekraanist välja
+    global height
+    global multiplier
+    height = n_rows - 4
     multiplier = current_row // height
     current_list = []
     j = 0
 
     for i in range(1, 4):
-        for el in sorted(current_files[i]):
+        # case-insensitive sorteerimine (vastasel juhul sorteeritakse suure algustähega failid/kaustad varem)
+        for el in sorted(current_files[i], key = lambda s: s.casefold()):
             current_list.append(el)
 
     # muutujad multiplier ja height aitavad üles ja alla liikuda, millal kausta sisu ei mahu vertikaalselt ekraanile.
-    # muutuja selected aitab järge hoida, et millise faili/kausta peal kasutaja on. selle abil toimub ka liikumine kaustast-kaussa.
+    # muutuja selected aitab järge hoida, et millise faili/kausta peal kasutaja on. selle abil toimub ka kaustade vaheline liikumine.
     for el in current_list[(height*multiplier):(height*(multiplier + 1))]:
         if j == current_row - multiplier*height:
+                # hoiab praeguse kohta failide/kaustada seast
                 global selected 
                 selected = el
+                el = short_el(el, n_cols)
                 main_window.addstr(j, 10, str(el), curses.color_pair(10))
         else:
             for i in range(1, 4):
                 if el in current_files[i]:
+                    el = short_el(el, n_cols)
                     main_window.addstr(j, 10, str(el), curses.color_pair(i))
         j += 1
             
@@ -85,31 +95,41 @@ def main(stdscr):
     # terminal-i akna suurus
     global n_rows, n_cols
     n_rows, n_cols = stdscr.getmaxyx()
+    current_row = 0
 
     # praeguse kausta failid
     global current_files
     current_files = files()
     menu_len = len(current_files[1]) + len(current_files[2]) + len(current_files[3])
 
-    # read, teksti pikkus, algus rida (lükkab alla), algus veerg (lükkab paremale)
-    main_window = curses.newwin(menu_len + 1, n_cols - 10, 2, 10)
-    head_window = curses.newwin(1, n_rows*3, 0, 0)
-
-    current_row = 0
+    # newwin(ridade arv, teksti pikkus, algus rida (lükkab alla), algus veerg (lükkab paremale))
+    # failide osa
+    main_window = curses.newwin(menu_len + 1, n_cols - 12, 2, 10)
+    # kausta nimi (põhimõttelist pwd)
+    head_window = curses.newwin(1, n_cols, 0, 0)
+    # kaustas oleva sisu maht
+    size_window = curses.newwin(0, n_cols + 1, n_rows-1, 1)
 
     stdscr.refresh()
     print_menu(main_window, current_row)
-
     
     while True:
         cwd = os.getcwd()
 
-        # head_window on praegu asukoha jaoks (põhimõtteliselt pwd)
         head_window.clear()
         head_window.addstr(0, 1, str(cwd), curses.color_pair(4))
         head_window.refresh()
         
-        # vastab ascii tabelile
+        size_window.clear()
+        # if-condition lisatud selleks, et tühja kausta oleks 0/0.
+        if menu_len == 0:
+            size_window.addstr(0, 0, str(0) + "/" + str(0), curses.color_pair(4))
+        else:
+            size_window.addstr(0, 0, str(current_row + 1) + "/" + str(menu_len), curses.color_pair(4))
+        
+        size_window.refresh()
+
+        # key väärtus vastab ascii tabelile
         key = stdscr.getch()
 
         # nool üles või k - liigub üles praeguses kaustas (kõige ülemise korral läheb alla)
@@ -119,7 +139,7 @@ def main(stdscr):
             else:
                 current_row = menu_len - 1
 
-        # nool alla või j - liigub alla praeguses kaustas (kõige madalama korral läheb üles)
+        # nool alla või j - liigub alla praeguses kaustas (kõige alumise korral läheb üles)
         elif key == curses.KEY_DOWN or key == 106:
             if current_row < menu_len - 1:
                 current_row += 1
@@ -138,9 +158,16 @@ def main(stdscr):
                 current_row = 0
 
                 menu_len = len(current_files[1]) + len(current_files[2]) + len(current_files[3])
-                main_window = curses.newwin(menu_len + 1, n_cols - 10, 2, 10)
+                main_window = curses.newwin(menu_len, n_cols - 10, 2, 10)
+            
+            # pildi puhul avatakse see image viewer-is (ei pruugi töötada kõigil juhtudel, mistõttu on juurde lisatud erand)
+            else:
+                try:
+                    (Image.open(os.getcwd() + "/" + selected)).show
+                except:
+                    pass
 
-        # nool vasakule või h - liigub tagasi failistruktuuris. kasutaja jääb valikuga samale kaustale, kust ta lahkus. vajutades pildil "h"-d, avatakse see image viewer-is (ei pruugi töötada kõigil juhtudel, mistõttu on juurde lisatud erand)
+        # nool vasakule või h - liigub tagasi failistruktuuris. kasutaja jääb valikuga samale kaustale, kust ta lahkus.
         elif key == curses.KEY_LEFT or key == 104:
             if os.path.isdir(os.getcwd() + "/" + ".."):
                 prev_dir = (os.getcwd()).split("/")
@@ -151,25 +178,23 @@ def main(stdscr):
                 current_files.clear()
                 current_files = files(new_path)
                 i = 0
-                for el in sorted(current_files[1]):
+                for el in sorted(current_files[1], key = lambda s: s.casefold()):
                     if el == prev_dir[-1]:
                         current_row = i
                         break
                     i += 1
 
                 menu_len = len(current_files[1]) + len(current_files[2]) + len(current_files[3])
-                main_window = curses.newwin(menu_len + 1, n_cols - 10, 2, 10)
-            else:
-                try:
-                    (Image.open(os.getcwd() + "/" + selected)).show
-                except:
-                    pass
+                main_window = curses.newwin(menu_len, n_cols - 10, 2, 10)
         
         # x - kustutab faili (küsib ka "nõrgalt" üle ehk ainult tähega "n" saab tegevust katkestada)
         elif key == 120:
             if selected in current_files[2] or selected in current_files[3]:
                 main_window.clear()
-                main_window.addstr(current_row, 10, str("delete " + selected + " [Y/n]"))
+                el_del = short_el(selected, n_cols)
+                main_window.addstr(current_row - height*multiplier, 2, str("delete: "))
+                main_window.addstr(current_row - height*multiplier, 10, str(el_del), curses.color_pair(10))
+                main_window.addstr(current_row - height*multiplier, 10 + len(el_del), "? [Y/n]")
                 main_window.refresh()
                 key = stdscr.getch()
                 # n
@@ -180,7 +205,8 @@ def main(stdscr):
                     current_files.clear()
                     current_files = files()
                     menu_len = len(current_files[1]) + len(current_files[2]) + len(current_files[3])
-                    main_window = curses.newwin(menu_len + 1, n_cols - 10, 2, 10)
+                    main_window = curses.newwin(menu_len, n_cols - 10, 2, 10)
+                    main_window.refresh()
 
         # g
         elif key == 103:
